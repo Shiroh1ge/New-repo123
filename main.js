206bb8f76fc3a9cc6e66c31caaa5322c0e09a245
ZerefeL <zerefel@gmail.com>
2015-09-12 21:14:00 +0300

    Added button to google on index page

---
+++ main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ZerefeL on 12/09/15.
+ */
+$(document).ready(function() {
+    $('#button-to-google').click(function() {
+       window.location.href = 'www.google.com';
+    });
+});
